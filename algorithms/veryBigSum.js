da23520905b655f42fa6a64fda2433556bd470b7
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2023-11-07 15:43:50 +0700

    refactor solution very big sum

--- algorithms/veryBigSum.js
+++ algorithms/veryBigSum.js
@@ -1,27 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'aVeryBigSum' function below.
  *
@@ -40,19 +16,6 @@ function aVeryBigSum(ar) {
   return sum;
 }
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const arCount = parseInt(readLine().trim(), 10);
-
-  const ar = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((arTemp) => parseInt(arTemp, 10));
-
-  const result = aVeryBigSum(ar);
-
-  ws.write(result + "\n");
-
-  ws.end();
-}
+console.log(
+  aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])
+);
