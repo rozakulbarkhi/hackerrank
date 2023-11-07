4607df929575ac88089944a2be2f6bdb0720f3a5
Rozakul Barkhi <rozakulbarkhirb@gmail.com>
2023-11-07 15:41:49 +0700

    refactor solution plus minus

--- algorithms/plusMinus.js
+++ algorithms/plusMinus.js
@@ -1,25 +1,3 @@
-"use strict";
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 /*
  * Complete the 'plusMinus' function below.
  *
@@ -48,13 +26,4 @@ function plusMinus(arr) {
   console.log(zero.length / length);
 }
 
-function main() {
-  const n = parseInt(readLine().trim(), 10);
-
-  const arr = readLine()
-    .replace(/\s+$/g, "")
-    .split(" ")
-    .map((arrTemp) => parseInt(arrTemp, 10));
-
-  plusMinus(arr);
-}
+plusMinus([-4, 3, -9, 0, 4, 1]);
